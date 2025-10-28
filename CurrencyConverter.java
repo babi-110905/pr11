9b86129d28827541b8288647443b1b41d94db87d
babi-110905 <babi110905@gmail.com>
2025-10-28 07:19:52 +0530

    Create CurrencyConverter.java

---
+++ CurrencyConverter.java
@@ -0,0 +1,7 @@
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        double inr = 100;
+        double usd = inr / 83;
+        System.out.println("100 INR = " + usd + " USD");
+    }
+}
